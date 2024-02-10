160384a4d98f1422d29edc3afbc280c78ac0d02f
RiccardoMont <riccardomontalto94@gmail.com>
2024-02-10 18:44:06 +0100

    Inserito loader

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,9 +7,25 @@ export default {
     name: 'AppMain',
     data() {
         return {
+            //Array dove poter inserire la lista di carte data dal response
             list: [],
+
+            //Array d'appoggio per creare dinamicamente le voci della select
             archetypes: [],
-            selectedArchetype: null
+
+            //Parametro per prendere il value del select durante l'utilizzo del filtro
+            selectedArchetype: null,
+
+            //Parametro per il v-show della barra dei risultati
+            results: false,
+
+            //Parametro per il conteggio del numero delle carte trovate dopo la ricerca effettuata
+            count: 0,
+
+            //V-show per il loader
+            loading: true
+
+            
         }
     },
     components: {
@@ -21,28 +37,43 @@ export default {
             .then((response) => {
 
                 this.list = response.data.data;
+                //Tolgo il loader una volta fatta la chiamata
+                this.loading = false;
 
+                //Ciclo per la creazione delle options nella select in modo dinamico
                 for (let i = 0; i < this.list.length; i++) {
 
+                    //Aggiunto parametro per il v-show ad ogni oggetto-carta
                     this.list[i].show = true;
 
+                    //Push dell'archetipo nell'array di supporto 
                     if (this.list[i].archetype) {
                         this.archetypes.push(this.list[i].archetype);
+
+                        //Organizzo le options in ordine alfabetico
                         this.archetypes.sort();
                     }
                 }
             })
     },
     methods: {
+        //Funzione della select
         archetypesFilter() {
-            console.log(this.selectedArchetype);
+
+            //Reimposto il contatore a 0 per evitare che si sommi con la ricerca precedente
+            this.count = 0;
+
+            //Filtro in azione che setta i parametri v-show del div-results e delle cards oltre che incrementare il parametro di conteggio dei risultati
             const filtro = this.list.filter(singlecard => {
                 if (this.selectedArchetype === 'All') {
                     singlecard.show = true;
+                    this.results = false;
                 } else if (this.selectedArchetype !== singlecard.archetype) {
                     singlecard.show = false;
                 } else {
                     singlecard.show = true;
+                    this.results = true;
+                    this.count++;
                 }
             })
         }
@@ -53,17 +84,23 @@ export default {
 <template>
     <div class="container">
         <div class="containerSelect">
-            <label for="DeckArchetype">Deck Archetype</label>
+            <label for="DeckArchetype">Deck Archetype:</label>
             <select @change="archetypesFilter()" v-model="selectedArchetype" name="deckArcs" id="DeckArchetype">
                 <option value="All" selected>All</option>
                 <option v-if="list != undefined" v-for="(singleArchetype, index) in new Set(archetypes)"
                     :value="singleArchetype">{{ singleArchetype }}</option>
             </select>
         </div>
-        <div class="containerCatalogue">
-            <div class="row">
+        <div class="containerCatalogue" >
+            <div v-show="this.results" class="results">
+                <span>Cards found: {{ this.count }}</span>
+            </div>
+            <div v-if="!loading" class="row">
                 <AppCardCatalogue v-if="list != undefined" v-for="(singlecard, index) in list" :singlecard="singlecard">
                 </AppCardCatalogue>
+            </div>
+            <div v-else class="loader">
+                <span>Loading...</span>
             </div>
         </div>
     </div>
@@ -71,17 +108,78 @@ export default {
 <style scoped>
 .containerSelect {
     padding: 2rem 0;
+
+    & label {
+        font-size: 20px;
+        font-weight: 700;
+    }
+
+    & select {
+        margin-left: 1rem;
+        padding: 0.5rem;
+        border-radius: 18px;
+    }
+
+    & select:active{
+        border-collapse: collapse ;
+        
+    }
 }
 
 .containerCatalogue {
     background-color: white;
     display: flex;
+    flex-direction: column;
     justify-content: center;
+    align-items: center;
     margin-bottom: 6rem;
+    border-radius: 1rem;
+
+    .loader {
+        & span{
+            font-size: 30px;
+            font-weight: 700;
+            animation: gradient 6s ease infinite;
+        }
+    }
+}
+
+.results {
+    width: calc(100% - 8rem);
+    background-color: black;
+    padding: 1rem 0.5rem;
+
+    & span {
+        color: white;
+        font-weight: 700;
+    }
 }
 
 .row {
     width: 100%;
-    padding: 3rem;
+    padding: 1rem 3rem 3rem 3rem;
 }
+
+
+@keyframes gradient {
+	0% {
+		color: black;
+	}
+    20% {
+        color: blue;
+    }
+	40% {
+		color: red;
+	}
+	60% {
+		color: darkorange;
+	}
+    80% {
+        color: green;
+    }
+    100% {
+        color: black;
+    }
+}
+
 </style>
